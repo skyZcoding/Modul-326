a530a05b684f8e0c2b042c8387df6d196cf9e9f8
Kenny Betschart <kbetschart2@online.gibz.ch>
2020-10-23 10:44:08 +0200

    Metro Framework Added Changed GUI

--- Battleship-Solitaire/Battleship-SolitaireUI/OptionsWindow.xaml.cs
+++ Battleship-Solitaire/Battleship-SolitaireUI/OptionsWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Battleship_SolitaireUI.Models.Option;
+using ControlzEx.Theming;
+using MahApps.Metro.Controls;
 using System.Windows;
 
 namespace Battleship_SolitaireUI
@@ -6,12 +8,20 @@ namespace Battleship_SolitaireUI
     /// <summary>
     /// Interaction logic for OptionsWindow.xaml
     /// </summary>
-    public partial class OptionsWindow : Window
+    public partial class OptionsWindow 
     {
         public OptionsWindow()
         {
             InitializeComponent();
             DataContext = Option.GetInstance();
+
+            //Change Window Theme to Dark and cyan coloured Titlebar
+            ThemeManager.Current.ChangeTheme(this, "Dark.Cyan");
+        }
+
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
