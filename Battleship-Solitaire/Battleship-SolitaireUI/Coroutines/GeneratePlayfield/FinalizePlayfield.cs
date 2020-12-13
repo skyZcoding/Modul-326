29b010fe0ac21a7208e43af798a884b761d24045
skyZ <jawamaga@gmail.com>
2020-12-13 04:02:56 +0100

    Remake of the command thingyy

---
+++ Battleship-Solitaire/Battleship-SolitaireUI/Coroutines/GeneratePlayfield/FinalizePlayfield.cs
@@ -0,0 +1,69 @@
+﻿using Battleship_SolitaireUI.Enums;
+using Battleship_SolitaireUI.Models.Option;
+using Battleship_SolitaireUI.Models.Playfield;
+using Battleship_SolitaireUI.ViewModels;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Battleship_SolitaireUI.Coroutines.GeneratePlayfield
+{
+    public class FinalizePlayfield : IResult
+    {
+        private readonly Playfield _playfield;
+        private readonly Option _option;
+        private readonly IEventAggregator _eventAggregator;
+        private readonly PlayfieldViewModel _playfieldViewModel;
+        public FinalizePlayfield()
+        {
+            _playfield = IoC.Get<Playfield>();
+            _option = IoC.Get<Option>();
+            _eventAggregator = IoC.Get<IEventAggregator>();
+            _playfieldViewModel = IoC.Get<PlayfieldViewModel>();
+        }
+
+        public void Execute(CoroutineExecutionContext context)
+        {
+            // amount of fields / 2 = amount which should be shown
+            int fieldCount = (int)((double)_playfield.Fields.Count / 100 * 70);
+            // Random Class (Check if field status is "turned over")
+            Random rnd = new Random();
+
+            // counts amount of fields that have been made visible
+            int changedFields = 0;
+            while (changedFields < fieldCount)
+            {
+                // gen y coordinate
+                int yCoord = rnd.Next(0, _option.Rows);
+                // gen x coordinate
+                int xCoord = rnd.Next(0, _option.Columns);
+
+                Field randomField = _playfield.Fields.First(f => f.XCoordinate == xCoord && f.YCoordinate == yCoord);
+
+                if (randomField.Status == FieldStatus.Unassigned)
+                {
+                    if (randomField.HasShipPiece)
+                    {
+                        randomField.Status = FieldStatus.Ship;
+                    }
+                    else if (!randomField.HasShipPiece)
+                    {
+                        randomField.Status = FieldStatus.Water;
+                    }
+
+                    changedFields++;
+                }
+                // else -> field has already been assigned, therefore, it is not interesting to us.
+            }
+
+            _eventAggregator.PublishOnUIThread(true);
+            _playfield.Status = Enums.PlayfieldStatus.InProgress;
+            _playfieldViewModel.Refresh();
+            Completed(this, new ResultCompletionEventArgs());
+        }
+
+        public event EventHandler<ResultCompletionEventArgs> Completed = delegate { };
+    }
+}
